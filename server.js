08ffa4220690e6406d960f40baa9bc438bfda91c
Terrel <TerrelRJones@gmail.com>
2021-10-11 22:27:09 -0700

    frontend added

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require("express");
-const { json, urlencoded } = require("express");
+const { json, urlencoded, static } = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
+const path = require("path");
 
 const sequelize = require("./models");
 const noteRouter = require("./routes/note.routes");
@@ -13,6 +14,12 @@ const dashboardRouter = require("./routes/dashboard.routes");
 const app = express();
 const port = process.env.PORT || 4001;
 
+app.use(static(path.join(__dirname, "client/build")));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(static(path.join(__dirname, "client/build")));
+}
+
 app.use(cors());
 app.use(json());
 app.use(urlencoded({ extended: true }));
@@ -23,6 +30,11 @@ app.use("/", loginRouter);
 app.use("/", dashboardRouter);
 app.use("/create", noteRouter);
 app.use("/", userRouter);
+
+// Catch all non-existent routes
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build/index.html"));
+});
 
 app.listen(port, async () => {
   console.log(`serving on port: ${port}`);
